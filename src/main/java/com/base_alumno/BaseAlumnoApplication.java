7f5b589a984577801c38c54410d125dd84ad3f66
Alfonso José Barroso Barajas <alfonsojbarroso@hotmail.com>
2026-09-10 17:56:12 -0600

    initial commit

---
+++ src/main/java/com/base_alumno/BaseAlumnoApplication.java
@@ -0,0 +1,13 @@
+package com.base_alumno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseAlumnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseAlumnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/base_alumno/BaseAlumnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base_alumno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseAlumnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
